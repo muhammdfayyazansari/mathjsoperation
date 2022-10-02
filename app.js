47f2afbc7cb8b2cdd30d88daa8b24d04abae8779
muhammadfayyazansari <fayyazansari268@gmail.com>
2022-10-02 20:06:54 +0500

    simplify

---
+++ app.js
@@ -0,0 +1,26 @@
+// function show(){
+// var aver = document.querySelector("#ave").value;    
+// var showAve = document.querySelector("#showave");
+// var findAve = math.median(Number(aver)).toString();
+// console.log(findAve) 
+// console.log(aver) 
+// console.log(typeof aver)  
+// }
+
+
+
+let log2 = ()=>{
+    let input = +document.querySelector("#log").value;
+    let show = document.querySelector('#resultLog');
+    let findLog = math.log2(input);
+    
+    show.innerHTML = "log<sub>2</sub> of "+input+' is '+ findLog;
+}
+
+let simplify = ()=>{
+    let input = document.querySelector("#doSimplify").value;
+    let show = document.querySelector('#resultSimplify');
+    let findSimplify = math.simplify(input).toString();
+    console.log(findSimplify)
+    show.innerHTML = "Simplify of "+input+' is ===== '+ findSimplify;
+}
